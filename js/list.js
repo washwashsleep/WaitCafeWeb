f372cd0a2876087bb6e0f0173d249ea04e48db93
Devin Chen <deseze74@gmail.com>
2015-07-26 00:49:39 +0800

    minor update

--- js/list.js
+++ js/list.js
@@ -1,13 +1,44 @@
 (function() {
-	var c = App.Collection({});
+	var Recommendation = App.Collection({
+		coordinates: App.Property({
+			defaultValue: '25.041399,121.554233'
+		}),
+		radius: App.Property({
+			defaultValue: 20
+		}),
+		limit: App.Property({
+			defaultValue: 5
+		}),
+		keyword: App.Property({
+			defaultValue: 'cafe'
+		}),
+		options: {
+			read: {
+				url: App.api + 'favorite/recommend'
+			}
+		}
+	});
+
+	var Favorite = App.Collection({
+		userId: App.Property({
+			defaultValue: App._userId
+		}),
+		options: {
+			read: {
+				url: App.api + 'favorite'
+			}
+		}
+	});
 
 	App.View('List', {
 		options: {
 			route: 'list/{{type}}',
 			url: 'pages/list.html'
 		},
+		recommendation: Recommendation(),
+		favorite: Favorite(),
 		routed: function(params) {
-			this.currentType = params.type;
+			this.list = this[params.type].read();
 		}
 	});
 }());
